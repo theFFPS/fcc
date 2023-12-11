52b7681db545c6c10fd76a72c61cede7d5484c6c
theFFPS <74585578+theFFPS@users.noreply.github.com>
2023-12-11 10:49:02 +0300

    Update BuildSystem.h

--- fcc-pm/inc/BuildSystem.h
+++ fcc-pm/inc/BuildSystem.h
@@ -8,10 +8,21 @@ struct Submodule {
     std::vector<std::map<std::string, std::vector<std::string>>> objs, outs;
     std::string module;
     std::vector<std::string> libDirs, incDirs;
+    inline void build() {
+        
+    }
 };
 struct BuildSystem {
     std::vector<Submodule> submodules;
     std::string project;
     std::map<std::string, std::string> settings;
+    inline void build() {
+        std::cout << "===> Started building " << project << "!\n";
+        for (Submodule submodule : submodules) {
+            submodule.settings = settings;
+            submodule.build();
+        }
+        std::cout << "===> Finished building " << project << "!\n";
+    }
 };
 #endif //__BUILDSYSTEM_FCCPM__
