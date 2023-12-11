879557fa2bbb3bd0ac4a10e7b705225b6167890f
theFFPS <74585578+theFFPS@users.noreply.github.com>
2023-12-11 10:56:55 +0300

    Update BuildSystem.h

--- fcc-pm/inc/BuildSystem.h
+++ fcc-pm/inc/BuildSystem.h
@@ -3,13 +3,23 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <thread>
+inline void buildObj(std::vector<std::string> const incDirs, std::map<std::string, std::string> const settings, std::map<std::string, std::vector<std::string>> const obj) {
+    
+}
 struct Submodule {
     std::map<std::string, std::string> settings;
     std::vector<std::map<std::string, std::vector<std::string>>> objs, outs;
     std::string module;
     std::vector<std::string> libDirs, incDirs;
     inline void build() {
-        
+        std::cout << "===> Started building " << module << "!\n";
+        for (std::map<std::string, std::vector<std::string>> obj : objs) {
+            std::cout << "===> Building " << obj["name"][0] << ".o\n";
+            std::thread t (buildObj, incDirs, settings, obj);
+            t.join();
+        }
+        std::cout << "===> Finished building " << module << "!\n";
     }
 };
 struct BuildSystem {
